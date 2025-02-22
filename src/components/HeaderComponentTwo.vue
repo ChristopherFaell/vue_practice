f1be68f8be44e4c4adfa4a8d0e0bdf40758db3bc
christopherFall <daevidpaerez@gmail.com>
2025-02-22 07:23:32 -0500

    Only One Layout

---
+++ src/components/HeaderComponentTwo.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import IconComponent.vue from './IconComponent.vue';
+  import LogoComponent from './LogoComponent.vue';
+  import ProfileComponent from './ProfileComponent.vue';
+</script>
+
+<template>
+<header>
+  <LogoComponent imageUrl="logo-lingui-academy.png"/>
+  <div class="flags">
+    <FlagsComponent imageUrl="reino-unido.png" />
+    <FlagsComponent imageUrl="francia.png" />
+    <FlagsComponent imageUrl="italia.png" />
+    <FlagsComponent imageUrl="alemania.png" />
+    <FlagsComponent imageUrl="brasil.png" />
+  </div>
+</header>
+</template>
---
+++ src/components/IconComponent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  const props = defineProps<{
+    imageUrl: string
+  }>()
+  const imageSrc = new URL(`../assets/${props.imageUrl}`, import.meta.url).href
+</script>
+
+<template>
+  <img :src="imageSrc" alt="Icon Images" class="icon-image" />
+</template>
+
+<style scoped>
+  .icon-image {
+    width: 35px;
+    height: 35px;
+  }
+</style>
